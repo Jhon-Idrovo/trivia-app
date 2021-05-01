25ea57c15a4abaffb848b28d673990f7e798a415
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-05-01 12:20:03 -0500

    l

--- portfolio/motorist-app/tailwind.config.js
+++ portfolio/motorist-app/tailwind.config.js
@@ -1,11 +1,23 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
+    colors: {
+      base: colors.gray[100],
+      "base-dark": colors.black,
+      primary: colors.yellow[300],
+
+      "txt-base": colors.white,
+      "txt-primary": colors.black,
+
+      secondary: colors.white,
+      "txt-primary": colors.black,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
