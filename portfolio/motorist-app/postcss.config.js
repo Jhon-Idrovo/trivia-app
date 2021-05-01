a001cfc53e41381fabb0df1962f4e87bffad4949
Jhon-Idrovo <77032831+Jhon-Idrovo@users.noreply.github.com>
2021-05-01 12:26:15 -0500

    config files added

--- portfolio/motorist-app/postcss.config.js
+++ portfolio/motorist-app/postcss.config.js
@@ -1,20 +1,6 @@
-const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-      base: colors.black,
-      primary: colors.yellow[300],
-      "txt-base": colors.white,
-      "txt-primary": colors.black,
-      secondary: colors.white,
-      "txt-primary": colors.black,
-    },
-    extend: {},
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 };
--- portfolio/motorist-app/tailwind.config.js
+++ portfolio/motorist-app/tailwind.config.js
@@ -1,11 +1,20 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
-  purge: [],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      base: colors.black,
+      primary: colors.yellow[300],
+      "txt-base": colors.white,
+      "txt-primary": colors.black,
+      secondary: colors.white,
+      "txt-primary": colors.black,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
